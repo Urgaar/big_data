d8bd3dd2fbbe3b5b610c323ef6a900f6098e7f32
Urgaar <82642331+Urgaar@users.noreply.github.com>
2024-03-28 17:39:30 +0100

    updatae

--- mapper_tags.py
+++ mapper_tags.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python
+from mrjob.job import MRJob
 
-import sys
+class TagCountPerMovie(MRJob):
 
-# Mapper pour trouver le nombre de tags par film et par utilisateur
-def mapper_default_config():
-    for line in sys.stdin:
+    def mapper(self, _, line):
         try:
-            # Splitter la ligne en userID, movieID, tagID, timestamp
-            userID, movieID, tagID, timestamp = line.strip().split(',')
-            # Émettre la clé 'movieID' avec valeur 1 pour chaque tag
-            print(f"{movieID}\t1")
-            # Émettre la clé 'userID' avec valeur 1 pour chaque utilisateur
-            print(f"{userID}\t1")
-        except Exception as e:
-            # Afficher les erreurs éventuelles
-            print("Erreur:", e, file=sys.stderr)
+            userID, movieID, tag, timestamp = line.split(',')
+            yield movieID, 1
+        except ValueError:
+            pass
 
-if __name__ == "__main__":
-    mapper_default_config()
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    TagCountPerMovie.run()
